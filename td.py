7bf178e112b8b3fddb6aee8c0c3fa1d44b99ca39
chillbro-insidejob <60394535+chillbro-insidejob@users.noreply.github.com>
2020-05-10 06:18:26 +0530

    Update td.py

--- td.py
+++ td.py
@@ -1,14 +1,14 @@
 import cv2
 import numpy as np
-camera = cv2.VideoCapture ("/root/New Folder/cars.mp4")
-camera.open("/root/New Folder/cars.mp4")
-car_cascade = cv2.CascadeClassifier('/root/New Folder/cars.xml')
+camera = cv2.VideoCapture ("/home/vineet/New Folder/cars.mp4")
+camera.open("/home/vineet/New Folder/cars.mp4")
+car_cascade = cv2.CascadeClassifier('/home/vineet/New Folder/cars.xml')
 while True:
     (grabbed,frame) = camera.read()
     grayvideo = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     cars = car_cascade.detectMultiScale(grayvideo, 1.1, 1)
     for (x,y,w,h) in cars:
-     cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+     cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
      cv2.imshow("video",frame)
     if cv2.waitKey(1)== ord('q'):
         break
